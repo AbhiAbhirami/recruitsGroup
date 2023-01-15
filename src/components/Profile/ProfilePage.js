d64cc1f4449cd37e94ea81e8682f7a203abbf997
ARSHADAMEEN00 <ameen.osperb@gmail.com>
2023-01-15 19:50:54 +0530

    ProfilePage

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "../Shared/Header";
+import PersonalDetails from "./components/PersonalDetails";
+import ProfileInfo from "./components/ProfileInfo";
 
 function ProfilePage() {
-	return <div>ProfilePage</div>;
+  return (
+    <div className="dashboard-main-cont">
+      <Header />
+      <div className="profile-section-cont">
+        <div className="profile-section-card">
+          <ProfileInfo />
+        </div>
+        <div className="profile-section-personal-details">
+          <PersonalDetails />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
