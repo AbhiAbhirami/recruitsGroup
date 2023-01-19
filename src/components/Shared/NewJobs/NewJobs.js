4ed60e20a7aeac08d8778da0227a1cfacf914008
Ameen Arshad Nediya <87312860+ARSHADAMEEN00@users.noreply.github.com>
2023-01-19 11:18:43 +0530

    google modal

--- src/components/Shared/NewJobs/NewJobs.js
+++ src/components/Shared/NewJobs/NewJobs.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import JobCard from './JobCard'
+import React from "react";
+import JobModal from "../JobModal/Jobmodal";
+import JobCard from "./JobCard";
 
 function NewJobs() {
-    return (
-        <div className='new-jobs-main-cont'>
-            <h3 className='new-jobs-head'>New Jobs</h3>
-            <div className='new-jobs-cards-cont'>
-                <JobCard />
-                <JobCard />
-                <JobCard />
-            </div>
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <>
+      <JobModal closeModal={() => setIsOpen(false)} isOpen={modalIsOpen} />
+      <div className="new-jobs-main-cont">
+        <h3 className="new-jobs-head">New Jobs</h3>
+        <div className="new-jobs-cards-cont">
+          <JobCard setIsOpen={setIsOpen} />
+          <JobCard />
+          <JobCard />
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default NewJobs
+export default NewJobs;
