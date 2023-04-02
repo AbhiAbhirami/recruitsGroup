f438182ac711840afefbb3563dffc7123e2b7351
ABHIRAMI P S <43600476+AbhiRami007@users.noreply.github.com>
2023-04-02 17:34:52 +0530

    Update socketIoContext.js

--- src/core/socketIoContext/socketIoContext.js
+++ src/core/socketIoContext/socketIoContext.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-const SOCKET_IO_URL= 'http://localhost:3001/'
+const SOCKET_IO_URL= 'https://crm.therecruitsgroup.com/'
 export const socket = io.connect(SOCKET_IO_URL);
 export const SocketContext = createContext();
